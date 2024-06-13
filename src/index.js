565c7555a860412c3b5781810a580764857f126c
Joe Bumbulis <joebumbulis@gmail.com>
2024-06-13 16:56:07 -0500

    adds working location based code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const location = request ? request.cf.city || request.cf.region || request.cf.country : "Austin";
+
+
+		const answer = await env.AI.run('@cf/meta/llama-3-8b-instruct', {
+			prompt: `Write a funny poem about ${location} in the form of a rap battle`,
+			stream: true
+		});
+
+		return new Response(answer, {
+			headers: { "content-type": "text/event-stream" }
+		});
+		// return new Response(JSON.stringify(answer));
+
 	},
 };
+
